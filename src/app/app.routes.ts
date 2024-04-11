c2b8f2df32ff17ce0036be7065ce1d17c4685a66
lucascidade <lucascidade96@gmail.com>
2024-04-11 17:29:10 -0400

    reorganizando projeto para o git

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignupComponent
+    }
+]
+
+;
