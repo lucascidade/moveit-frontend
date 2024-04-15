a25f9d4008dd3a777005708b3a208ef23cf590bd
lucascidade <lucascidade96@gmail.com>
2024-04-15 10:47:42 -0400

    feat: adicionado authguard para pagina de usuaro autenticado

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -45,7 +45,7 @@ export class SignupComponent {
   }
 
   submit(){
-    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
+    this.loginService.signup(this.signupForm.value.name, this.signupForm.value.email, this.signupForm.value.password).subscribe({
       next: () => this.toastService.success("login feito com sucesso!"),
       error: () => this.toastService.error("Erro inesperado!")
     })
